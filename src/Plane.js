e3613b6123ed87e950baa4d60da8b19d27403cd6
christymasterson <christymasterson@gmail.com>
2020-01-31 13:52:20 +0000

    Added failing unit test for airport can clear planes for take off

--- src/Plane.js
+++ src/Plane.js
@@ -1,6 +1,14 @@
 
 class Plane {
+  constructor() {
+    this._location;
+  }
   landPlane(airport) {
     airport.clearForLanding(this)
+    this._location = airport
+  };
+
+  takeOffPlane() {
+    this._location.clearForTakeOff(this)
   };
 };

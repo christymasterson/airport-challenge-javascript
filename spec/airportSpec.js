3cf90cf59ae4b5d3b6e0d5121d7bb9ab00fb6ea3
christymasterson <christymasterson@gmail.com>
2020-01-31 12:04:02 +0000

    Created failing unit test for airport that says it is initially empty

---
+++ spec/airportSpec.js
@@ -0,0 +1,11 @@
+describe('Airport', function(){
+  var airport;
+
+  beforeEach(function(){
+    airport = new Airport;
+  })
+
+  it('airport is initially empty', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+});

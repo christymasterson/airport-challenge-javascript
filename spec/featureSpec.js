50d8b9c2d85e9297309d299de675593f8b4b1ec3
christymasterson <christymasterson@gmail.com>
2020-01-31 13:43:06 +0000

    Created failing feature test for plane take off

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -13,4 +13,10 @@ describe('Feature test', function(){
     plane.landPlane(airport);
     expect(airport.planes()).toContain(plane);
   });
+
+  it('planes take off from an airport', function(){
+    plane.landPlane(airport);
+    plane.takeOffPlane(airport);
+    expect(airport.planes()).not.toContain(plane);
+  });
 });

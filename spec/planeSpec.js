7c22d95156c57ce33c402a99873ef67baea65947
christymasterson <christymasterson@gmail.com>
2020-01-31 12:21:52 +0000

    Added test to check plane can interact with airport. Stubbed airport

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -2,14 +2,21 @@
 
 describe("Plane", function() {
   var plane;
-
+  var airport;
+  
   beforeEach(function() {
     plane = new Plane();
+    airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
   });
 
-    describe("landing plane", function() {
-      it('responds to land plane', function(){
-        expect(plane.landPlane).not.toBeUndefined()
-      });
+  describe("landing plane", function() {
+    it('responds to land plane', function(){
+      expect(plane.landPlane).not.toBeUndefined()
     });
+
+    it('can land at an airport', function(){
+      plane.land(airport);
+      expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+    });
+  });
 });

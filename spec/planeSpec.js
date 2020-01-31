b71c54350372e29b31b77921246dfa31f34838ba
christymasterson <christymasterson@gmail.com>
2020-01-31 11:18:34 +0000

    First failing test, responds to land plane function

---
+++ spec/planeSpec.js
@@ -0,0 +1,13 @@
+describe("Plane", function() {
+  var plane;
+
+  beforeEach(function() {
+    plane = new Plane();
+  });
+
+    describe("landing plane", function() {
+      it('responds to land plane', function(){
+        expect(plane.landPlane).not.toBeUndefined()
+      });
+    });
+});
